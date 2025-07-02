1361ac716a1f5a63e2f78dc799def4510f4b85d7
SpectraSolar0 <leo040016@gmail.com>
2025-07-02 09:39:05 +0100

    Update fiches.js

--- fiches.js
+++ fiches.js
@@ -2,6 +2,6 @@ module.exports = {
    Gustavo: { nom: 'Vargas', prenom: 'Gustavo', age: 35, cheveux: 'Châtain foncé', yeux: 'Bleu', numero: '?', braquages: 0, ventes: 0, avertissements: 0 },
   Angel: { nom: 'Saucedo', prenom: 'Angel', age: 31, cheveux: 'Marron', yeux: 'Bleu', numero: '(602) 726 7178', braquages: 0, ventes: 0, avertissements: 0 },
   Emilio: { nom: 'Vasquez', prenom: 'Emilio', age: 27, cheveux: 'Noir/Gris', yeux: 'Châtain claire', numero: '(520) 167 8503', braquages: 0, ventes: 0, avertissements: 0 },
-  Saucedo: { nom: 'Juan', prenom: 'Saucedo', age: 29, cheveux: 'Noir', yeux: 'Noir', numero: '(205) 275 5892', braquages: 0, ventes: 0, avertissements: 0 },
+  Juan: { nom: 'Saucedo', prenom: 'Juan', age: 29, cheveux: 'Noir', yeux: 'Noir', numero: '(205) 275 5892', braquages: 0, ventes: 0, avertissements: 0 },
   Alexandro: { nom: 'Martes', prenom: 'Alexandro', age: 22, cheveux: 'Noir', yeux: 'Noir', numero: '(602) 779 2280', braquages: 0, ventes: 0, avertissements: 0 }
 };
